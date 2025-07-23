41d33b3d68793f2d076dbf0b448f71fec3407f32
25Dark25 <166438142+25Dark25@users.noreply.github.com>
2025-07-23 02:25:55 +0200

    Update stealabrainrotscipt.lua

--- stealabrainrotscipt.lua
+++ stealabrainrotscipt.lua
@@ -26,18 +26,15 @@ local function createButton(parent, text, size, position, color)
     btn.Size = size
     btn.Position = position
     btn.Text = text
-    btn.TextColor3 = Color3.new(1, 1, 1)
+    btn.TextColor3 = Color3.fromRGB(0, 0, 0) -- en vez de blanco
     btn.BackgroundColor3 = color
     btn.AutoLocalize = false
     return btn
 end
 
 local closeButton = createButton(mainFrame, "X", UDim2.new(0, 30, 0, 30), UDim2.new(1, -35, 0, 5), Color3.fromRGB(255, 0, 0))
-closeButton.TextColor3 = Color3.fromRGB(0, 0, 0)
 local minimizeButton = createButton(mainFrame, "-", UDim2.new(0, 30, 0, 30), UDim2.new(1, -70, 0, 5), Color3.fromRGB(200, 200, 200))
-minimizeButton.TextColor3 = Color3.fromRGB(0, 0, 0)
 local toggleESPButton = createButton(mainFrame, "Disable ESP", UDim2.new(0.8, 0, 0, 40), UDim2.new(0.1, 0, 0.5, 0), Color3.fromRGB(200, 200, 200))
-toggleESPButton.TextColor3 = Color3.fromRGB(0, 0, 0)
 
 local minimizedBar = createButton(screenGui, "Dark", UDim2.new(0, 80, 0, 30), UDim2.new(0.5, -40, 0, 10), Color3.fromRGB(40, 40, 40))
 minimizedBar.Visible = false
