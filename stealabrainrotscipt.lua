1b46ae8136c52f3db716047c138a50658896212b
25Dark25 <166438142+25Dark25@users.noreply.github.com>
2025-07-25 14:55:47 +0200

    Update stealabrainrotscipt.lua

--- stealabrainrotscipt.lua
+++ stealabrainrotscipt.lua
@@ -23,7 +23,7 @@ screenGui.DisplayOrder = 10000
 
 local mainFrame = Instance.new("Frame", screenGui)
 mainFrame.Name = "MainFrame"
-mainFrame.Size = UDim2.new(0, 300, 0, 200)
+mainFrame.Size = UDim2.new(0, 250, 0, 200)
 mainFrame.Position = UDim2.new(0.5, -125, 0.5, -100)
 mainFrame.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 
